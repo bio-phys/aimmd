29ab133af59ea483c3175837b904ab5739fcfd09
hejung <hendrik.andre.jung@gmail.com>
2019-03-31 14:00:26 +0200

    pytorch.FFNet: now possible to set number of hidden units per layer

--- arcd/pytorch/networks.py
+++ arcd/pytorch/networks.py
@@ -29,25 +29,35 @@ import torch.nn.functional as F
 
 
 class FFNet(nn.Module):
-    """
-    Simple feedforward network with 4 hidden layers.
-    n_in - number of input coordinates
-    n_out - number of log probabilities to output,
-            i.e. 1 for 2-state and N for N-State,
-            make sure to use the correct loss
-    """
-    def __init__(self, n_in, n_out=1, activation=F.elu, **kwargs):
+    """Simple feedforward network with 4 hidden layers."""
+
+    def __init__(self, n_in, n_out=1, n_hidden=None, activation=F.elu,
+                 **kwargs):
+        """
+        Initialize FFNet.
+
+        n_in - number of input coordinates
+        n_out - number of log probabilities to output,
+                i.e. 1 for 2-state and N for N-State,
+                make sure to use the correct loss
+        n_hidden - number of hidden units per layer,
+                   list of ints or None, if None default to n_in
+        activation - the torch activation function used for all hidden layers
+        """
         super().__init__()
         self.call_kwargs = {'n_in': n_in,
                             'n_out': n_out,
-                            'activation': activation
+                            'activation': activation,
+                            'n_hidden': n_hidden
                             }
+        if n_hidden is None:
+            n_hidden = [n_in for _ in range(4)]
         self.activation = activation
-        self.lay0 = nn.Linear(n_in, n_in)
-        self.lay1 = nn.Linear(n_in, n_in)
-        self.lay2 = nn.Linear(n_in, n_in)
-        self.lay3 = nn.Linear(n_in, n_in)
-        self.out_lay = nn.Linear(n_in, n_out)
+        self.lay0 = nn.Linear(n_in, n_hidden[0])
+        self.lay1 = nn.Linear(n_hidden[0], n_hidden[1])
+        self.lay2 = nn.Linear(n_hidden[1], n_hidden[2])
+        self.lay3 = nn.Linear(n_hidden[2], n_hidden[3])
+        self.out_lay = nn.Linear(n_hidden[3], n_out)
 
     def forward(self, x):
         x = self.activation(self.lay0(x))
