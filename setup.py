d3b3203398ab2cbe5647ddeb0597668adc092863
hejung <hendrik.andre.jung@gmail.com>
2020-04-07 21:39:43 +0200

    really removed py v3.4

--- setup.py
+++ setup.py
@@ -148,7 +148,6 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.4',  # CI tested
         'Programming Language :: Python :: 3.5',  # CI tested
         'Programming Language :: Python :: 3.6',  # CI tested, prodcution use
         'Programming Language :: Python :: 3.7',  # CI tested, production use
