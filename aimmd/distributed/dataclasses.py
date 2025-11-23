ca1410477e640e95227d7ec11b3e0d1bf27517b1
hejung <hendrik.andre.jung@gmail.com>
2025-11-23 17:33:29 +0100

    Add license comment

--- aimmd/distributed/dataclasses.py
+++ aimmd/distributed/dataclasses.py
@@ -1,3 +1,17 @@
+# This file is part of aimmd
+#
+# aimmd is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# aimmd is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with aimmd. If not, see <https://www.gnu.org/licenses/>.
 """
 This file contains various dataclasses used to tie together input arguments
 commonly used together, such as the MDEngineSpec.
